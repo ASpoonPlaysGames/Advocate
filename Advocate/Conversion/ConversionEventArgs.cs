4c734f072f55c522ae188cc812f848c05cd78ac9
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2022-11-15 15:28:50 +0000

    doc comments for MessageType enum and members

--- Advocate/Conversion/ConversionEventArgs.cs
+++ Advocate/Conversion/ConversionEventArgs.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace Advocate.Conversion
 {
+    /// <summary>
+    ///     Holds the type of message,
+    ///     used for determining behaviour when logging
+    /// </summary>
     public enum MessageType
     {
+        /// <summary>
+        ///     <para>Indicates that the message should only
+        ///     be shown when running in Debug.</para>
+        ///     Not shown in gui.
+        /// </summary>
         Debug,
+        /// <summary>
+        ///     <para>Default message type.</para>
+        ///     <para>For general purpose messages to the user in gui and console.</para>
+        ///     Shown in gui. See <see cref="MainWindow.MainWindow_OnConversionMessage"/> for implementation.
+        /// </summary>
         Info,
+        /// <summary>
+        ///     <para>Indicates that the skin conversion is complete.</para>
+        ///     Shown in gui. See <see cref="MainWindow.MainWindow_OnConversionComplete"/> for implementation.
+        /// </summary>
         Completion,
+        /// <summary>
+        ///     <para>Indicates an error during skin conversion.</para>
+        ///     Shown in gui. See <see cref="MainWindow.MainWindow_OnConversionError"/> for implementation.
+        /// </summary>
         Error
     }
     public class ConversionMessageEventArgs : EventArgs
