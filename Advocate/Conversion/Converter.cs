76bc4afe08552cb0e1629c34676f4d9a46ecdf25
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2022-11-21 23:44:12 +0000

    return correct value on unknown error

--- Advocate/Conversion/Converter.cs
+++ Advocate/Conversion/Converter.cs
@@ -582,7 +582,7 @@ namespace Advocate.Conversion
 
 				// exit out of the conversion
 				Error("Unknown Error!");
-				return true;
+				return false;
 			}
 
 			// everything is done and hopefully good
