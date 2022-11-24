92a505e7b8eecd47c30ff2be941b615884c87007
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2022-11-24 19:56:37 +0000

    formatting apparently

--- Advocate/Conversion/JSON/Map.cs
+++ Advocate/Conversion/JSON/Map.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 namespace Advocate.Conversion.JSON
 {
